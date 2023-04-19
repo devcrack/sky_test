dd8bfbf9edf4cc0b8d4113af5dd1bcc3fc24114b
devcrack <aurelio.hdz@yandex.com>
2023-04-19 15:51:56 -0600

    Added controllers and providers to Test Module

--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { TestController } from './test.controller';
+import { TestService } from './test.service';
 
 @Module({
   imports: [HttpModule],
+  controllers: [TestController],
+  providers: [TestService],
 })
 export class TestModule {}
