e6115c01e826af5c314a01376a06f6cab6c6c504
devcrack <aurelio.hdz@yandex.com>
2023-04-20 13:56:07 -0600

    docs: documenting controller

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -3,10 +3,22 @@ import { TestService } from './test.service';
 
 // Value passed to Controller decorator is the initial path for the URLs of this
 // module
+
+/**
+ * The TestController handles HTTP requests related to test data.
+ */
 @Controller('test')
 export class TestController {
+  /**
+   * Creates a new TestController instance.
+   * @param testService The TestService instance to be injected.
+   */
   constructor(private testService: TestService) {}
 
+  /**
+   * Handles GET requests to the /test/users URL and returns test user data.
+   * @returns The test user data.
+   */
   @Get('users')
   get_users() {
     return this.testService.getSomething();
