84e98507394febe84c4b5bb056d7fc600ac24589
devcrack <aurelio.hdz@yandex.com>
2023-04-21 14:56:53 -0600

    perf: Improvement we remove one foreach loop using TypeScript power.

--- src/test/test.service.ts
+++ src/test/test.service.ts
@@ -40,7 +40,7 @@ export class TestService {
    * @throws {Error} If there is an error retrieving or processing the data.
    * @returns {Promise<any[]>} An array of objects containing the processed data.
    */
-  async getSomething() {
+  async getUsers() {
     try {
       // Define the URI for the external API.
       const api_uri = 'https://jsonplaceholder.typicode.com/users';
@@ -51,36 +51,25 @@ export class TestService {
       const { data } = await firstValueFrom(this.httpService.get(api_uri));
 
       // Sort the data array by ID in descending order.
-      const sorted_by_id = data.sort((a, b) => b.id - a.id);
+      const sortedById = data.sort((a, b) => b.id - a.id);
 
-      // Initialize an array to hold the processed data.
-      const returned_data: any[] = [];
-
-      // Iterate over each data item in the sorted array.
-      sorted_by_id.forEach((data_item) => {
+      const returnedData = sortedById.map((data_item) => {
         // Create a new empty object to hold the processed data item
         const nw_obj = {};
-
         // Iterate over each property in the data item using the 'Object.keys' method.
-        Object.keys(data_item).forEach((key) => {
-          // If the property is not 'address', add it to the new object.
-          if (key !== 'address') {
-            // console.log(key, data_item[key]);
-            nw_obj[key] = data_item[key];
-          }
-        });
+        Object.keys(data_item)
+          .filter((item) => item != 'address')
+          // Add the processed data item to the returned data array.
+          .forEach((key) => (nw_obj[key] = data_item[key]));
 
-        // Add the processed data item to the returned data array.
-        returned_data.push(nw_obj);
+        return nw_obj;
       });
 
       // Before to leave
-      const users_with_even_id = returned_data.filter(
-        (user) => user.id % 2 == 0,
-      );
-      await this.publishUsers(JSON.stringify(users_with_even_id));
+      const usersWithEvenId = returnedData.filter((user) => user.id % 2 == 0);
+      await this.publishUsers(JSON.stringify(usersWithEvenId));
       // Return the processed data array.
-      return returned_data;
+      return returnedData;
     } catch (error) {
       // If there is an error retrieving or processing the data, throw a new error with a custom message.
       throw new Error(`Error getting data: ${error.message}`);
